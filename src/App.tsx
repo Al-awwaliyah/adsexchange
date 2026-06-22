234d18fd7a2ff1b37f412aed501fae8f36bada09
Oyelere Abdullahi <lawalaremu53@gmail.com>
2026-06-22 13:09:27 +0100

    Remove Navbar import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Index from "./pages/Index";
