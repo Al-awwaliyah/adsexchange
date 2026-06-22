978084484ccd9ba25090ebd03af15a229dd4cff1
Oyelere Abdullahi <lawalaremu53@gmail.com>
2026-06-22 12:49:49 +0100

    Add Navbar and footer to App component

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <AuthProvider>
+
+          <Navbar />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
@@ -42,6 +44,8 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
+
+          <footer/>
         </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
